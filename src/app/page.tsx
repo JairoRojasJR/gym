6d7591201ba583917028a62de9a5613e157bf636
Jairo Rojas <jairorojasofficial@gmail.com>
2023-07-11 17:32:13 -0500

    fix configuracion lint-staged

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home(): JSX.Element {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <h1>Gym Sitio Web</h1>
+      <h1> Gym Sitio Web</h1>
     </main>
   )
 }
