aa172633e4a62e63f3f2423eb80797d48a5451e5
Jairo Rojas <jairorojasofficial@gmail.com>
2023-07-12 20:13:18 -0500

    landing

---
+++ src/app/components/Cover.tsx
@@ -0,0 +1,25 @@
+export default function Cover(): JSX.Element {
+  return (
+    <div className='h-[100vh] w-full'>
+      <video
+        className='absolute h-full w-full object-cover'
+        autoPlay={true}
+        loop={true}
+        preload='auto'
+        muted
+      >
+        <source src='motivacion_deportiva_x.mp4' type='video/mp4' />
+      </video>
+      <section className='relative z-[100] flex h-full w-full items-center justify-center'>
+        <div className='flex flex-col gap-6'>
+          <p className='text-center text-2xl'>
+            Por una vida saludable y un cuerpo mamado
+            <br />
+            <b className='text-6xl'>Supérate</b>
+          </p>
+          <button className='bg-[#5DC1B9] text-3xl'>OBTENER MEMBRESÍA</button>
+        </div>
+      </section>
+    </div>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,33 @@
+import Image from 'next/image'
+import Cover from './components/Cover'
+import Login from './components/svg/Login'
+import Dumbbell from './components/svg/Dumbbell'
+import Link from 'next/link'
+
 export default function Home(): JSX.Element {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <h1> Gym Sitio Web</h1>
+    <main className='flex min-h-screen flex-col items-center justify-between'>
+      <nav className='fixed top-0 z-[200] flex w-full min-w-[300px] flex-col items-center gap-2 p-5 sm:flex-row sm:justify-between'>
+        <div className='flex items-center'>
+          <Image alt='Gym Machala' src='/favicon.ico' width={50} height={50} />
+          <h1 className='text-3xl font-bold text-[#FEB3C2]'>SuperSayaGym</h1>
+        </div>
+        <ul className='flex items-center gap-4'>
+          <li className='flex items-center'>
+            <Link href='#' className='bg-[#FEB3C2] p-2 text-slate-800'>
+              Iniciar sesión
+            </Link>
+            <Login color='#FEB3C2' />
+          </li>
+          <li className='flex items-center'>
+            <Link href='#' className='bg-[#FEB3C2] p-2 text-slate-800'>
+              Máquinas
+            </Link>
+            <Dumbbell color='#FEB3C2' />
+          </li>
+        </ul>
+      </nav>
+      <Cover />
     </main>
   )
 }
